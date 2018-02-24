b982bf076f71f7872dab1a42ac01aaaca875970f
nz_brian <brianban@gmail.com>
2018-02-24 16:36:01 +1300

    removed need for email and worker. Made single mine mode and mport 0

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/cuda:/usr/lib/nvidia
 
-if [[ -z "$WORKER" || -z "$EMAIL" || -z "EWAL" ]]; then
-    echo "You must at least set WORKER, EMAIL, and EWAL env vars."
+if [[ -z "EWAL" ]]; then
+    echo "You must at least set EWAL env var."
     exit 1
 fi
 
-echo "POOL: ${EPOOL}, WALLET: ${EWAL}/${WORKER}/${EMAIL}, PSW: ${EPSW}, WORKER: ${WORKER}, ESM: ${ESM}, ALLPOOLS: 0" > /claymore/epools.txt
-echo "POOL: ${DPOOL}, WALLET: ${DWAL}/${WORKER}/${EMAIL}, PSW: ${DPSW}, WORKER: ${WORKER}, ESM: 0, ALLPOOLS: 0" > /claymore/dpools.txt
+echo "POOL: ${EPOOL}, WALLET: ${EWAL}, PSW: ${EPSW}, ESM: ${ESM}, ALLPOOLS: 0" > /claymore/epools.txt
 
 /claymore/ethdcrminer64 \
+    -mport 0
+    -mode 1
     -epool ${EPOOL} \
-    -ewal ${EWAL}/${WORKER}/${EMAIL} \
+    -ewal ${EWAL} \
     -epsw ${EPSW} \
     -ethi ${ETHI} \
     -esm ${ESM} \
-    -dcoin ${DCOIN} \
-    -dpool ${DPOOL} \
-    -dwal ${DWAL}/${WORKER}/${EMAIL} \
-    -dpsw ${DPWD} \
-    -dcri ${DCRI} \
-    -di ${DI} \
     -gser ${GSER} \
     -dbg 0
+    -allpools 0
