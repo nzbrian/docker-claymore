1389db0ab48290333990c9c85e9a2809d870b5e2
Brian Bannister <brianban@gmail.com>
2018-02-24 18:51:13 +1300

    now runs

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,14 +8,14 @@ fi
 
 echo "POOL: ${EPOOL}, WALLET: ${EWAL}, PSW: ${EPSW}, ESM: ${ESM}, ALLPOOLS: 0" > /claymore/epools.txt
 
-/claymore/ethdcrminer64 \
-    -mport 0
-    -mode 1
+/claymore/Claymore\'s\ Dual\ Ethereum+Decred_Siacoin_Lbry_Pascal\ AMD+NVIDIA\ GPU\ Miner\ v11.0\ -\ LINUX/ethdcrminer64 \
+    -mport 0 \
+    -mode 1  \
     -epool ${EPOOL} \
     -ewal ${EWAL} \
     -epsw ${EPSW} \
     -ethi ${ETHI} \
     -esm ${ESM} \
     -gser ${GSER} \
-    -dbg 0
+    -dbg 0  \
     -allpools 0
